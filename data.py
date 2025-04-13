57da6019b8f7d2f3dbaf56f07d030b3eda24e3d2
jjxrry <114961129+jjxrry@users.noreply.github.com>
2025-04-13 14:10:02 -0700

    add boilerplate

---
+++ data.py
@@ -0,0 +1,18 @@
+# recipe list
+recipes = [
+    [name, cuisine, [ingredients]], #type:ignore
+]
+
+# fridge only contains ingredients
+fridge_one = [
+    [name, quality, amount],  #type:ignore
+]
+
+fridge_two = [
+
+]
+
+fridge_three = [
+
+]
+
